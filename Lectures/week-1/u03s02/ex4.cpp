86bdcd2571d3ade50c49186c3f832d8e7c3d0cbd
Salar Farahmand <24586486+wolf1128@users.noreply.github.com>
2026-03-08 00:16:39 +0100

    feat: added ex4

---
+++ Lectures/week-1/u03s02/ex4.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file ex4.cpp
+ * @author Salar Farahmand
+ * @brief 
+ *          Objective: Cpp basics.
+ *          Reference: This example addresses the page#15 of u03s02 C++ Basics slide.
+ * @version 0.1
+ * @date 2026-03-07
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    // TODO: Hellow world (v2)
+    int v1, v2;
+
+    cout << "Enter two values: ";
+    cin >> v1 >> v2;
+    cout << "Sum: " << v1+v2 << endl;
+
+
+
+    return 0;
+}
