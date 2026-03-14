a6e53f853a97749d2d605a51ecae4c66d365a468
Salar Farahmand <24586486+wolf1128@users.noreply.github.com>
2026-03-14 13:03:22 +0100

    feat: added example04

---
+++ Lectures/week-3/s03s04/example04.cpp
@@ -0,0 +1,41 @@
+/**
+ * @file ex1_v2.cpp
+ * @author Salar Farahmand
+ * @brief 
+ *          Objective: C++ Library > class
+ *          Reference: #17 of u03s04
+ * @version 0.1
+ * @date 2026-03-14
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Employee {
+    private:
+        int salary;
+    public:
+        // setter
+        void setSalary(int s) {
+            salary = s;
+        }
+        // getter
+        int getSalar() {
+            return salary;
+        }
+};
+
+int main() {
+    Employee myObj;
+    myObj.setSalary(5000);
+
+    cout << myObj.getSalar() << "\n";
+
+
+    return 0;   
+ }
