ab0c3a8f161da64a2069a604e8e1149cb649b089
Salar Farahmand <24586486+wolf1128@users.noreply.github.com>
2026-03-14 12:59:55 +0100

    feat: added example03

---
+++ Lectures/week-3/s03s04/example03.cpp
@@ -0,0 +1,36 @@
+/**
+ * @file ex1_v2.cpp
+ * @author Salar Farahmand
+ * @brief 
+ *          Objective: C++ Library > class
+ *          Reference: #16 of u03s04
+ * @version 0.1
+ * @date 2026-03-14
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class my_class {
+    public:
+        void my_method();
+};
+
+// method definiton outside of the class
+void my_class::my_method() {
+    cout << "Hello world!";
+}
+
+
+ int main() {
+    my_class my_obj;
+    my_obj.my_method();
+
+
+    return 0;   
+ }
